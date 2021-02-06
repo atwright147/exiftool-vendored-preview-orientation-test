370f32784518e9d7e8b69b1084e8ea652d7821a4
Andy Wright <atwright147@gmail.com>
2021-02-06 12:54:06 +0000

    attempt to extract bigger images

--- index.ts
+++ index.ts
@@ -5,14 +5,27 @@ import { DefaultExiftoolArgs, ExifTool } from 'exiftool-vendored';
 
 const configPath = path.resolve('./my.config');
 const sourceImagePath = path.resolve('./DSC01005.ARW');
-const destImagePath = path.resolve('./out.jpg');
+const destImagePath = path.resolve('./extracted');
 
 console.info({ configPath, sourceImagePath, destImagePath });
 
 (async () => {
   const et = new ExifTool({exiftoolArgs: ['-config', configPath, ...DefaultExiftoolArgs]});
+
   try {
-    await et.extractBinaryTag('-RotatedThumbnail', sourceImagePath, destImagePath);
+    await et.extractBinaryTag('-RotatedJpegFromRaw', sourceImagePath, path.join(destImagePath, 'JpegFromRaw.jpg'));
+  } catch (err) {
+    console.error(err);
+  }
+
+  try {
+    await et.extractBinaryTag('-RotatedPreview', sourceImagePath, path.join(destImagePath, 'preview.jpg'));
+  } catch (err) {
+    console.error(err);
+  }
+
+  try {
+    await et.extractBinaryTag('-RotatedThumbnail', sourceImagePath, path.join(destImagePath, 'thumbnail.jpg'));
   } catch (err) {
     console.error(err);
   }
